1824e3fe3fe01b6c866019c75265ed12b8c9f0bd
jerry denis <denisoyola360@gmail.com>
2024-02-15 17:14:12 -0500

    add librarys service

---
+++ app/src/main/java/com/example/peliculasapp/common/network/api/Endpoints.kt
@@ -0,0 +1,13 @@
+package com.example.moviesapp.common.network.api
+
+object Endpoints {
+
+    const val API_KEY = "73c41ccc8dabcddcaea54412b63991f6"
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500"
+
+    const val IMAGE_ALTO = 200
+    const val IMAGE_ANCHO = 120
+
+    const val MAX_CALIFICACION = 10.0
+}
