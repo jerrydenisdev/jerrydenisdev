62e498da4026fae67e261e41cf2bcc1b136a433a
jerry denis <denisoyola360@gmail.com>
2024-02-15 17:09:04 -0500

    estructurando package model mvvm

--- app/src/main/java/com/example/peliculasapp/views/MainActivity.kt
+++ app/src/main/java/com/example/peliculasapp/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.peliculasapp
+package com.example.peliculasapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.peliculasapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
